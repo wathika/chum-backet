a901c5286fc865a5939f5bf9ce12a13a53908da5
wathika <vcwathika@gmail.com>
2015-09-24 02:54:20 -0700

    php file to generate xml

---
+++ foodstore.php
@@ -0,0 +1,8 @@
+<?php
+header('Content_Type: text/xml');
+echo '<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>';
+?>
+
+
+
+<!-- we need a php file to generate an xml file and then send the file to the clients computer -->
