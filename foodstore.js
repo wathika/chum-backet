bf6885791f0ce3a93651bb45fefead6dac2a6fb0
wathika <vcwathika@gmail.com>
2015-09-24 03:50:45 -0700

    sending requests to the server

--- foodstore.js
+++ foodstore.js
@@ -23,14 +23,18 @@ function createXmlHttpRequestObject() {
 		return xmlHttp;
 }
 
+
 //this process function is going to be responsible for taking an object and sending the request to the server 
 function process(){
-
+//is the object ready??
 	if (xmlHttp.readyState == 0 || xmlHttp.readyState == 4) {
 		//our object is ready to communicate with the server
 		food = encodeURIComponent(document.getElementByid("userInput").value); //whatever the user typed in the box
-		
-	}else{
+		xmlHttp.open("GET", "foodstore.php?food=" + food, true);   //my request
+		xmlHttp.onreadystatechange = handleServerResponse;   //call the function handleServerResponse whenever the server responds
+		xmlHttp.send(null);    //sending the request
 
+	}else{
+		setTimeout("process()", 1000);    //
 	}
 }
