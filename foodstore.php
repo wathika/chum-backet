0b62582c97935f15694ba4fd147ea3d2a760ff87
wathika <vcwathika@gmail.com>
2015-09-24 03:05:42 -0700

    created xml content

--- foodstore.php
+++ foodstore.php
@@ -1,6 +1,21 @@
 <?php
 header('Content_Type: text/xml');
 echo '<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>';
+
+echo "<response>";
+
+	$food = $_GET["food"];
+	$foodArray = array('tuna','bacon','beef','loaf','ugali','kuku','yorguht');
+	if(in_array($food, $foodArray))
+		echo "Awesome Choice, we do have ".$food. "!";
+	elseif ($food=="") 
+		echo "Enter a food you IDIOT!!"
+	else
+		echo "Sorry Punk, we don't sell no " .$food. "!";
+
+echo "<response>";
+
+
 ?>
 
 
